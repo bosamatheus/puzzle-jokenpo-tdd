ed60ac568ddfcf30e1a51fef8439b4327ad539b1
Matheus Bosa <matheusbozza@gmail.com>
2020-03-02 15:59:01 -0300

    Finaliza TDD

--- jokenpo.py
+++ jokenpo.py
@@ -1,15 +1,70 @@
 import unittest
 
-class Jokenpo(object):
+
+class Jokenpo():
     
-    def verifica_vencedor(self, jogador1, jogador2):
-        return "Pedra"
+    def joga(self, jogador1, jogador2):
+        if jogador1 == "Pedra":
+            if jogador2 == "Tesoura":
+                return "Pedra"
+            elif jogador2 == "Papel":
+                return "Papel"
+            else:
+                return "Pedra"
+        elif jogador1 == "Papel":
+            if jogador2 == "Pedra":
+                return "Papel"
+            elif jogador2 == "Tesoura":
+                return "Tesoura"
+            else:
+                return "Papel"
+        else:
+            if jogador2 == "Pedra":
+                return "Pedra"
+            elif jogador2 == "Papel":
+                return "Tesoura"
+            else:
+                return "Tesoura"
+
 
 class JokenpoTest(unittest.TestCase):
 
-    def test_pedra_vence(self):
+    def test_pedra_tesoura(self):
         jokenpo = Jokenpo()
-        self.assertEqual("Pedra", jokenpo.verifica_vencedor("Pedra", "Tesoura"))
+        self.assertEqual("Pedra", jokenpo.joga("Pedra", "Tesoura"))
+
+    def test_pedra_papel(self):
+        jokenpo = Jokenpo()
+        self.assertEqual("Papel", jokenpo.joga("Pedra", "Papel"))
+    
+    def test_pedra_pedra(self):
+        jokenpo = Jokenpo()
+        self.assertEqual("Pedra", jokenpo.joga("Pedra", "Pedra"))
+
+    def test_papel_tesoura(self):
+        jokenpo = Jokenpo()
+        self.assertEqual("Tesoura", jokenpo.joga("Papel", "Tesoura"))
+    
+    def test_papel_pedra(self):
+        jokenpo = Jokenpo()
+        self.assertEqual("Papel", jokenpo.joga("Papel", "Pedra"))
+    
+    def test_papel_papel(self):
+        jokenpo = Jokenpo()
+        self.assertEqual("Papel", jokenpo.joga("Papel", "Papel"))
+    
+    def test_tesoura_pedra(self):
+        jokenpo = Jokenpo()
+        self.assertEqual("Pedra", jokenpo.joga("Tesoura", "Pedra"))
+    
+    def test_tesoura_papel(self):
+        jokenpo = Jokenpo()
+        self.assertEqual("Tesoura", jokenpo.joga("Tesoura", "Papel"))
+    
+    def test_tesoura_tesoura(self):
+        jokenpo = Jokenpo()
+        self.assertEqual("Tesoura", jokenpo.joga("Tesoura", "Tesoura"))
+
 
 if __name__ == "__main__":
     unittest.main()
